c5cd5704802c814a1f50f1849c74a13fba9b6d72
M-Klemm <matthias_klemm@gmx.net>
2024-03-02 15:42:16 +0100

    Change: key name of the inverter serial number changed in the config file (old config file still works though)
    Add: support for up to 9 inverters connected to a single OpenDTU

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # OpenDTU to pvoutput.org
 Small utility to read data from an OpenDTU device and upload it to pvoutput.org.
 Fill in the necessary info in the config file before running the script.
-Only a single inverter is supported at the moment. If you have multiple inverters or multiple OpenDTUs,
-copy this script and its (modified) config file to a different folder for each inverter / OpenDTU. 
+Up to 9 inverters, connected to a single OpenDTU, are currently supported. Add 'serial_number2', 'serial_number3', ... 
+to the 'openDTU' section in the config file. Power and energy yield of all inverters will be summed up before uploading to pvoutput.org.
+If you have multiple inverters connected to a single OpenDTU and each inverter should have its data uploaded 
+to a separate system at pvoutput.org or if you have multiple OpenDTUs, copy this script and its (modified) config file 
+to a different folder for each inverter / OpenDTU. 
 Optionally, additional parameters, such as e.g. inverter temperature, voltage / current / power of each string, can also be uploaded.
 
 The script will try up to 10 times to connect to the OpenDTU device and obtain valid data.
@@ -11,7 +14,7 @@ The script will try up to 10 times to connect to the OpenDTU device and obtain v
 
 OpenDTU 24.2.12 or later is required.
 Python 3.2 or later is required to run the script.
-Use cron (linux) or the task scheduler (Windows) to run the script every 5 minutes 
+Use cron (linux) or the task scheduler (Windows) to run the script every 5 minutes. 
 
 
 # Required python modules
@@ -29,19 +32,19 @@ log_level=ERROR                 # possible log levels: 'CRITICAL', 'ERROR', 'WAR
                                 # there are no 'CRITICAL' errors defined -> 'CRITICAL' will result in an empty log file
 [openDTU]
 ip=X.X.X.X                      # OpenDTU IP address
-sn=XXXXXXXXXX                   # inverter serial number
+serial_number1=XXXXXXXXXX       # inverter #1 serial number
 
 [pvoutput]
 pvo_apikey=XXXXXXXXXX           # API key from pvoutput.org
 pvo_systemid=XXXX               # ID of the system to upload to
 pvo_upload_temperature=true     # if true, upload inverter temperature 
 pvo_upload_voltage=true         # if true, upload line voltage
-pvo_v7=DC.0.Power               # optional parameter
-pvo_v8=DC.1.Power               # optional parameter
-pvo_v9=DC.0.Voltage             # optional parameter
-pvo_v10=DC.0.Voltage            # optional parameter
-pvo_v11=DC.0.YieldDay           # optional parameter
-pvo_v12=DC.1.YieldDay           # optional parameter
+pvo_v7=serial_number1.DC.0.Power        # optional parameter
+pvo_v8=serial_number1.DC.1.Power        # optional parameter
+pvo_v9=serial_number1.DC.0.Voltage      # optional parameter
+pvo_v10=serial_number1.DC.0.Voltage     # optional parameter
+pvo_v11=serial_number1.DC.0.YieldDay    # optional parameter
+pvo_v12=serial_number1.DC.1.YieldDay    # optional parameter
 pvo_single_url=https://pvoutput.org/service/r2/addstatus.jsp?key=       # do NOT edit
 ```
 
--- openDTU2PVO.py
+++ openDTU2PVO.py
@@ -3,7 +3,7 @@
 #
 # @file     openDTU2PVO.py
 # @author   Matthias Klemm <Matthias_Klemm@gmx.net>
-# @version  1.0.0
+# @version  1.1.0
 # @Python   >= 3.2 required
 # @date     February, 2024
 #
@@ -69,64 +69,95 @@ except Exception as err:
     logging.error('IP address: \'%s\' is not valid', str(openDTU_ip))
     print('IP address: \'' + str(openDTU_ip) + '\' is not valid.')
     sys.exit(1)
-openDTU_sn = int(configParser.get('openDTU', 'sn'))
 
-# read current pv data from the openDTU
-currentValues = ''
-maxRetries = 10
-requestStr = "http://" + openDTU_ip + "/api/livedata/status?inv=" + str(openDTU_sn)
-for retryCounter in range(1, maxRetries, 1):
-    logging.debug('openDTU communication try #%d', retryCounter)
-    response = requests.get(requestStr)
-    logging.debug("Sent request to openDTU: %s", requestStr)
-    if response.status_code != 200:
-        # request was not successful -> wait a bit and retry
-        logging.warning("Could not get data from openDTU: %s", response.text)
-        time.sleep(10)
+DTUData = {}
+totalPower = 0
+totalEnergy = 0
+allDataTooOldFlag = True
+nInv = len(configParser.options('openDTU'))
+for invNumber in range(1, nInv, 1):
+    try:
+        openDTU_sn = int(configParser.get('openDTU', 'serial_number' + str(invNumber)))
+    except Exception as err:
+        if invNumber == 1:
+            # try old config file format
+            try:
+                openDTU_sn = int(configParser.get('openDTU', 'sn'))
+            except Exception as err2:
+                logging.error('Could not find serial_number1 in openDTU section of config.cfg')
+                print('Could not find serial_number1 in openDTU section of config.cfg')
+                sys.exit(1)
+        else:
+            # the requested serial_number does not exist in config.cfg
+            continue
+    # read current pv data from the openDTU
+    currentValues = ''
+    maxRetries = 10
+    requestStr = "http://" + openDTU_ip + "/api/livedata/status?inv=" + str(openDTU_sn)
+    for retryCounter in range(1, maxRetries, 1):
+        logging.debug('openDTU communication try #%d', retryCounter)
+        response = requests.get(requestStr)
+        logging.debug("Sent request to openDTU: %s", requestStr)
+        if response.status_code != 200:
+            # request was not successful -> wait a bit and retry
+            logging.warning("Could not get data from openDTU: %s", response.text)
+            time.sleep(10)
+            continue
+        else:
+            currentValues = json.loads(response.content)
+            break
+    # sanity check
+    if not currentValues:
+        # something went wrong -> nothing to do ->exit
+        logging.error("Failed to receive data from openDTU device")
+        print("Failed to receive data from openDTU device")
         continue
-    else:
-        currentValues = json.loads(response.content)
-        break
-# sanity check
-if not currentValues:
-    # something went wrong -> nothing to do ->exit
-    logging.error("Failed to receive data from openDTU device")
-    print("Failed to receive data from openDTU device")
+    if 'inverters' not in currentValues:
+        # no inverter data found
+        tmpStr = "Could not get data from an inverter " + str(invNumber) + "- skipping..."
+        logging.error(tmpStr)
+        print(tmpStr)
+        continue
+    if len(currentValues['inverters']) == 0:
+        # no inverter data found
+        tmpStr = "Could not get data from inverter - exiting..."
+        logging.error(tmpStr)
+        print(tmpStr)
+        continue
+    invData = currentValues['inverters'][0]
+    if 'data_age' in invData:
+        allDataTooOldFlag = allDataTooOldFlag and (invData['data_age'] > 60)
+    if 'AC' not in invData:
+        # no AC data from inverter
+        tmpStr = "Received no AC data from inverter - exiting..."
+        logging.warning(tmpStr)
+        print(tmpStr)
+        continue
+    if 'DC' not in invData:
+        # no DC data from inverter
+        tmpStr = "Received no DC data from inverter - exiting..."
+        logging.warning(tmpStr)
+        print(tmpStr)
+        continue
+    if 'INV' not in invData:
+        # no INV data from inverter
+        tmpStr = "Received no INV data from inverter - exiting..."
+        logging.warning(tmpStr)
+        print(tmpStr)
+        continue
+    # sanity checks passed
+    DTUData['serial_number' + str(invNumber)] = invData
+    totalPower += invData['AC']['0']['Power']['v']
+    if invData['AC']['0']['Power']['v'] > 0:
+        # the daily energy counter is reset, once power is fed into the grid
+        # -> if power is still zero, the energy yield is from yesterday -> ignore it
+        totalEnergy += invData['INV']['0']['YieldDay']['v']
+
+if not DTUData:
     sys.exit(1)
-if 'inverters' not in currentValues:
-    # no inverter data found
-    tmpStr = "Could not get data from an inverter - exiting..."
-    logging.error(tmpStr)
-    print(tmpStr)
-    sys.exit(1)
-if len(currentValues['inverters']) == 0:
-    # no inverter data found
-    tmpStr = "Could not get data from inverter 0 - exiting..."
-    logging.error(tmpStr)
-    print(tmpStr)
-    sys.exit(1)
-invData = currentValues['inverters'][0]
-if 'data_age' not in invData or invData['data_age'] > 60:
-    # inverter data too old
-    tmpStr = "Data from inverter 0 older than 60 seconds - exiting..."
-    logging.warning(tmpStr)
-    print(tmpStr)
-    sys.exit(1)
-if 'AC' not in invData:
-    # no AC data from inverter
-    tmpStr = "Received no AC data from inverter - exiting..."
-    logging.warning(tmpStr)
-    print(tmpStr)
-    sys.exit(1)
-if 'DC' not in invData:
-    # no DC data from inverter
-    tmpStr = "Received no DC data from inverter - exiting..."
-    logging.warning(tmpStr)
-    print(tmpStr)
-    sys.exit(1)
-if 'INV' not in invData:
-    # no INV data from inverter
-    tmpStr = "Received no INV data from inverter - exiting..."
+if allDataTooOldFlag:
+    # data from all inverters too old
+    tmpStr = "Data from inverter all inverters older than 60 seconds"
     logging.warning(tmpStr)
     print(tmpStr)
     sys.exit(1)
@@ -134,18 +165,18 @@ if 'INV' not in invData:
 # upload data to pvoutput.org
 now = datetime.now()  # current date and time
 uploadStr = configParser.get('pvoutput', 'pvo_single_url') + configParser.get('pvoutput', 'pvo_apikey') + "&sid=" + configParser.get('pvoutput', 'pvo_systemid') + "&d=" + str(now.strftime("%Y%m%d")) + "&t=" + str(
-    now.strftime("%H:%M")) + "&c1=0" + "&v1=" + str(int(invData['INV']['0']['YieldDay']['v'])) + "&v2=" + str(invData['AC']['0']['Power']['v'])
+    now.strftime("%H:%M")) + "&c1=0" + "&v1=" + str(int(totalEnergy)) + "&v2=" + str(totalPower)
 # handle optional pvoutput.org parameters
-if bool(configParser.get('pvoutput', 'pvo_upload_temperature')):
-    uploadStr = uploadStr + "&v5=" + str(invData['INV']['0']['Temperature']['v'])
-if bool(configParser.get('pvoutput', 'pvo_upload_voltage')):
-    uploadStr = uploadStr + "&v6=" + str(invData['AC']['0']['Voltage']['v'])
+if bool(configParser.get('pvoutput', 'pvo_upload_temperature')) and 'serial_number1' in DTUData:
+    uploadStr = uploadStr + "&v5=" + str(DTUData['serial_number1']['INV']['0']['Temperature']['v'])
+if bool(configParser.get('pvoutput', 'pvo_upload_voltage'))  and 'serial_number1' in DTUData:
+    uploadStr = uploadStr + "&v6=" + str(DTUData['serial_number1']['AC']['0']['Voltage']['v'])
 for i in range(7, 13, 1):
     tmp = configParser.get('pvoutput', 'pvo_v' + str(i))
     if isValidString(tmp):
         try:
             tmp = tmp.split('.')
-            uploadStr = uploadStr + "&v" + str(i) + "=" + str(int(invData[tmp[0]][tmp[1]][tmp[2]]['v']))
+            uploadStr = uploadStr + "&v" + str(i) + "=" + str(int(DTUData[tmp[0]][tmp[1]][tmp[2]][tmp[3]]['v']))
         except Exception as err:
             logging.warning("Optional pvoutput parameter pvo_v%d failed: %s", i, err)
             continue
